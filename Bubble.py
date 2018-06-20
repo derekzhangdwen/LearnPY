375475a596704a18676953a89a7fbde06042be24
derekzhangdwen <derekzdw@outlook.com>
2018-06-20 15:46:58 +0800

    follow the book

--- Bubble.py
+++ Bubble.py
@@ -1,7 +1,7 @@
 def bubble(a):
     n=len(a)
     for i in range(n):
-        for j in range(i+1,n):
-            if a[j]<a[i]:
+        for j in range(n):
+            if a[j]>a[i]:
                 a[j],a[i]=a[i],a[j]
     return a
