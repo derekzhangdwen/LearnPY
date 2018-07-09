e4e38f0c79efe569b4ac4542daaeaad4086a2e3f
derekzhangdwen <derekzdw@outlook.com>
2018-07-09 13:37:02 +0800

    First working Inverse Modulo function

--- NumTheo.py
+++ NumTheo.py
@@ -4,42 +4,24 @@ def gcd(a,b):
     return b
 
 def invmod(a,b):
-    ta,tb=a,b
+    #ta,tb=a,b
     trace=[]
     switch=False
-    l,r=1,1
-    flag=True
+    l,r=1,0
     while a!=0:
-        print(a,b)#
+        #print(a,b)#
         trace.append(b//a)
         a,b=b%a,a
     if b!=1:
         raise ValueError('invalid input: gcd(%d,%d)!=1' %(a,b))
-    print(trace)#
+    #print(trace)#
     if trace[0]==0:
         switch=True
         del trace[0]
-    if len(trace)==2:
-        if switch:
-            return(1,-trace[0])
-        else:
-            return (-trace[0],1)
-    else:
-        del trace[len(trace)-1]
-        del trace[len(trace)-1]
-        for i in range(len(trace)-1,-1,-1):
-            if flag:
-                l=l+r*trace[i]
-                flag=not flag
-            else:
-                r=r+l*trace[i]
-                flag=not flag
-        if len(trace)%2==0:
-            l,r=-r,-l
-        if switch:
-            print(ta*(-r)+tb*l)#
-            return(-r,l)
-
-        else:
-            print(ta*l+tb*(-r))#
-            return(l,-r)
+    #del trace[len(trace)-1]
+    for i in range(len(trace)-2,-1,-1):
+        l,r=r-l*trace[i],l
+    if switch:
+        l,r=r,l
+    #print(ta*l+tb*r)#
+    return(l,r)
